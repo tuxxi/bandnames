5de930f43e010f795cfe907f072222695461f522
tuxxi <aidan.sojourner@gmail.com>
2018-07-01 21:46:52 -0700

    Initial project

---
+++ app/src/main/java/me/sojourner/bandnames/MainActivity.kt
@@ -0,0 +1,18 @@
+package me.sojourner.bandnames
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button_1.setOnClickListener {
+            textView.text = "Hello Kotlin World!"
+        }
+    }
+
+}
