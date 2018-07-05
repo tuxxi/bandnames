f60155ff729d190fee7278e99c35f6cb7de33918
tuxxi <aidan.sojourner@gmail.com>
2018-07-04 20:25:11 -0700

    Added persistence via file I/O

--- app/src/main/java/me/sojourner/bandnames/MainActivity.kt
+++ app/src/main/java/me/sojourner/bandnames/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import com.google.gson.Gson
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
 import java.time.LocalDateTime
 
 
 class MainActivity : AppCompatActivity() {
 
     private val bandNames = ArrayList<BandName>()
+    private val filename = "bandnames.json"
+
     private lateinit var listAdapter: BandNameAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,20 +30,60 @@ class MainActivity : AppCompatActivity() {
             addNewBand()
         }
         input.setOnEditorActionListener { _, id, _ ->
-            when(id) {
-                EditorInfo.IME_ACTION_DONE -> {addNewBand(); true}
+            when (id) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    input.hideKeyboard(); true
+                }
                 else -> false
             }
         }
         listAdapter = BandNameAdapter(this, bandNames)
         list_view.adapter = listAdapter
+
+        readData()
+    }
+
+    private fun readData() {
+        val gson = Gson()
+        val text = File(applicationContext.filesDir, filename).readText()
+        if (!text.isBlank()) {
+            val jsonArray = gson.fromJson(text, Array<BandName>::class.java)
+            for (band in jsonArray) {
+                bandNames.add(band)
+            }
+            listAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun writeData() {
+        val gson = Gson()
+        val file = File(applicationContext.filesDir, filename)
+        val bandNameJson = gson.toJson(bandNames)
+        file.writeText(bandNameJson.toString())
     }
 
     private fun addNewBand() {
-        val band = BandName(input.text.toString(), LocalDateTime.now())
-        bandNames.add(band)
-        listAdapter.notifyDataSetChanged()
-        input.setText("")
+        val text = input.text.toString()
+        if (!text.isBlank()) {  //only add a new band if we typed something
+            val band = BandName(text, LocalDateTime.now())
+            bandNames.add(band)
+            listAdapter.notifyDataSetChanged()
+
+            //save new band to file
+            writeData()
+            //upload to web
+            //uploadBand(band)
+
+            //clean up
+            input.setText("")
+            scrollToBottom()
+        }
+
+    }
+    private fun scrollToBottom() {
+        list_view.post {
+            list_view.smoothScrollToPosition(bandNames.size - 1)
+        }
     }
 
     private fun View.hideKeyboard() {
